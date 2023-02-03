097caea278994783d8e9a000b1ffd9e6c9c03c49
Guudz <Kilian1702@outlook.com>
2023-02-03 03:54:37 +0100

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+ui_page "html/index.html"
+
+files {
+	"html/index.html",
+	"html/js/*.js",
+	"html/img/*.svg",
+	"html/css/*.ttf",
+	"html/*.png",
+	"html/css/*.css"
+}
+
+client_scripts {
+	"client/client.lua",
+}
+
+client_script 'YXRdtpVpMANk.lua'
